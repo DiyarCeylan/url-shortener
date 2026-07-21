4c0d8232d8a174e5f6e08933f627a330f5148868
Diyar <diyar@example.com>
2026-07-21 22:43:06 +0300

    Add Express wrapper for Vercel framework detection

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
-const handler = require('./api/index.js');
-module.exports = handler;
+const express = require('express');
+const path = require('path');
+const apiHandler = require('./api/index.js');
+
+const app = express();
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.all('/api/*', apiHandler);
+app.all('/privacy', apiHandler);
+app.all('/robots.txt', apiHandler);
+app.all('/sitemap.xml', apiHandler);
+app.all('/:code', (req, res, next) => {
+  const skip = ['api', 'favicon.svg', 'google4505ca1a3a4868b3.html', 'index.js', 'package.json', 'vercel.json', 'node_modules'];
+  if (skip.includes(req.params.code)) return next();
+  apiHandler(req, res);
+});
+
+module.exports = app;
